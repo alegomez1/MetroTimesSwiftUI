f8034c2acff7bc3b4fde9da76509965a1a228376
Alejandro Gomez <alegomez1@me.com>
2021-06-27 23:02:48 -0400

    Initial Commit

---
+++ MetroTimes/MetroTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetroTimesApp.swift
+//  MetroTimes
+//
+//  Created by Alejandro Gomez on 6/27/21.
+//
+
+import SwiftUI
+
+@main
+struct MetroTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
