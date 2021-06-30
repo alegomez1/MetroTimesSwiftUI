1edd1e7da7f8b76f0d2696db65bfac78edda7f2a
Alejandro Gomez <alegomez1@me.com>
2021-06-30 15:32:40 -0400

    Added design changes

--- MetroTimes/Views/ContentView.swift
+++ MetroTimes/Views/ContentView.swift
@@ -14,35 +14,93 @@ struct ContentView: View {
     var body: some View {
         
 
-        ZStack {
-            VStack {
-                Text(model.stationName)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.center)
-                    
-                VStack{
-                    Text("Northbound Trains")
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                    HStack() {
-                        Text(model.NB_train1)
-                        Text(model.NB_train2)
-                    }
+        VStack(alignment: .center) {
+                ZStack {
+//                    RoundedRectangle(cornerRadius: 25.0, style: .circular)
+//                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+//                        .frame(width: 275, height: 70)
+                    Text(model.stationName)
+                        .foregroundColor(.black)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
                 }
-                VStack{
-                    Text("Southbound Trains")
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                    HStack() {
-                        Text(model.SB_train1)
-                        Text(model.SB_train2)
-                    }
-                }
-            }
+
+            VStack{
+                Text("Northbound Trains")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                ZStack{
+                    RoundedRectangle(cornerRadius: 15.0, style: .circular)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.061, brightness: 0.91))
+                        .frame(width: 350, height: 80)
             
-        }.frame(maxWidth: .infinity, maxHeight: .infinity)
-        .ignoresSafeArea()
+                    HStack() {
+                        VStack(alignment: .center){
+                            Text("1st Train")
+                                .font(.headline)
+                                .padding(.bottom, 5)
+                            Text(model.NB_train1)
+                        }
+                        .padding(.trailing, 55)
+                        
+                        VStack(alignment: .center){
+                            Text("2nd Train")
+                                .font(.headline)
+                                .padding(.bottom, 5)
+                            Text(model.NB_train2)
+                        }
+                        .padding(.leading, 55)
+                    }
+                }
+                .padding(.bottom, 50)
+                
+                Text("Southbound Trains")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                ZStack{
+                    RoundedRectangle(cornerRadius: 15.0, style: .circular)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.061, brightness: 0.91))
+                        .frame(width: 350, height: 80)
+            
+                    HStack() {
+                        VStack(alignment: .center){
+                            Text("1st Train")
+                                .font(.headline)
+                                .padding(.bottom, 5)
+                            Text(model.SB_train1)
+                        }
+                        .padding(.trailing, 55)
+                        
+                        VStack(alignment: .center){
+                            Text("2nd Train")
+                                .font(.headline)
+                                .padding(.bottom, 5)
+                            Text(model.SB_train2)
+                        }
+                        .padding(.leading, 55)
+                    }
+                }
+                
+                Button(action: {
+                    print("Refresh button")
+                }, label: {
+                    Text("Refresh")
+                        .fontWeight(.bold)
+                        .font(.title3)
+                        .foregroundColor(.purple)
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.purple, lineWidth: 5))
+                })
+                .padding(.top, 100)
+
+            }
+            .padding(.top, 50)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment:.top)
+            
+            }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
         
     }
     
